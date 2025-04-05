b60c482f615f69b2ab527ac604eac8827e621298
ParzivalPy <gabriel.arthus@icloud.com>
2025-04-05 17:46:14 +0200

    Enhance footer content and styling; adjust layout and spacing for better readability

--- footer.php
+++ footer.php
@@ -4,16 +4,43 @@
             <h2>
                 Qui suis-je
             </h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lobortis venenatis tincidunt. Fusce auctor
+                hendrerit turpis, et efficitur augue vehicula eget. Aliquam ac luctus justo. Curabitur lacinia turpis
+                nisi, id cursus nulla consectetur nec. Suspendisse lacus nulla, imperdiet et tincidunt in, suscipit id
+                nisi. Fusce quam velit, tempus sit amet sem pharetra, pharetra malesuada nibh. Praesent sed finibus mi.
+                Suspendisse eu metus dapibus, efficitur magna a, fermentum eros. Etiam vulputate massa elit, id
+                dignissim risus cursus vitae.</p>
         </div>
         <div class="footer-center footer-part">
             <h2>
                 Me Contacter
             </h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lobortis venenatis tincidunt. Fusce auctor
+                hendrerit turpis, et efficitur augue vehicula eget. Aliquam ac luctus justo. Curabitur lacinia turpis
+                nisi, id cursus nulla consectetur nec. Suspendisse lacus nulla, imperdiet et tincidunt in, suscipit id
+                nisi. Fusce quam velit, tempus sit amet sem pharetra, pharetra malesuada nibh. Praesent sed finibus mi.
+                Suspendisse eu metus dapibus, efficitur magna a, fermentum eros. Etiam vulputate massa elit, id
+                dignissim risus cursus vitae.</p>
         </div>
         <div class="footer-right footer-part">
             <h2>
                 Plus d'informations
             </h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lobortis venenatis tincidunt. Fusce auctor
+                hendrerit turpis, et efficitur augue vehicula eget. Aliquam ac luctus justo. Curabitur lacinia turpis
+                nisi, id cursus nulla consectetur nec. Suspendisse lacus nulla, imperdiet et tincidunt in, suscipit id
+                nisi. Fusce quam velit, tempus sit amet sem pharetra, pharetra malesuada nibh. Praesent sed finibus mi.
+                Suspendisse eu metus dapibus, efficitur magna a, fermentum eros. Etiam vulputate massa elit, id
+                dignissim risus cursus vitae.</p>
+        </div>
+    </div>
+    <div class="copyrights">
+        <p>Copyright © 2025 - Tous droits réservés</p>
+        <div class="socials">
+            <a href="https://www.facebook.com/"><img src="/assets/images/facebook.png" alt="facebook"></a>
+            <a href="https://www.instagram.com/"><img src="/assets/images/instagram.png" alt="instagram"></a>
+            <a href="https://www.linkedin.com/"><img src="/assets/images/linkedin.png" alt="linkedin"></a>
+            <a href="https://www.twitter.com/"><img src="/assets/images/twitter.png" alt="twitter"></a>
         </div>
     </div>
 </footer>
